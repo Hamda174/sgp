c56d50265d921df92ed9512561ef924b00a28561
Hamda174 <hammda174@gmail.com>
2025-03-05 20:39:59 +0400

    Update flakskriskrate.py

--- flakskriskrate.py
+++ flakskriskrate.py
@@ -14,6 +14,13 @@ import openpyxl
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
+if __name__ == "__flaskriskrate__":
+    app.run()
+
 # Define the function to process data
 def process_data():
     file_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRnH-_FyYUJ5NCF_HHQjT1JhCGl7MsMxRlsRWVib3wi7P78LHuDgkLk2RwjlcuXNQ/pub?output=csv"
